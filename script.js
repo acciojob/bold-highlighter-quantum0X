3c53ef44a09f3c19b4b49c01bdea21afaf4958ca
Vishal Singh <96664216+quantum0X@users.noreply.github.com>
2025-02-22 14:20:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const strongTags = document.querySelectorAll('strong')
+
 function highlight() {
     //Write your code here
-
-
+	for (let index = 0; index < strongTags.length; index++) {
+		strongTags[index].style.color = 'rgb(0, 128, 0)'
+	}
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	for (let index = 0; index < strongTags.length; index++) {
+		strongTags[index].style.color = 'rgb(0, 0, 0)'
+	}
 }
